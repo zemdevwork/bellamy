9582919eea5a737dd83969e847fe1dbfba35d8e7
ashitha <vmashitha3@gmail.com>
2025-09-09 15:28:17 +0530

    chore: setup shadcn/ui and refactor components

--- src/app/pages/Homepage/page.tsx
+++ src/app/pages/Homepage/page.tsx
@@ -1,6 +1,6 @@
-import Header from "../../components/Header";
-import Banner from "../../components/Banner";
-import ProductList from "../../components/ProductList";
+import Header from "../../../components/Header";
+import Banner from "../../../components/Banner";
+import ProductList from "../../../components/ProductList";
 
 export default function Homepage() {
   return (
