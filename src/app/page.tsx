b07666cd4fc4005333c985e3cc77331085a6496f
shibili.P <shibiliwillowy@gmail.com>
2025-09-13 15:17:52 +0530

    footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import ProductList from "../components/ProductList";
+import Footer from "../components/Footer";
 
 export default function Homepage() {
   return (
@@ -8,6 +9,7 @@ export default function Homepage() {
       <Header />
       <Banner />
       <ProductList />
+      <Footer />
     </div>
   );
 }
