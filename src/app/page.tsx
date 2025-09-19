32178964dbe39557a473801a81f949dae0b84096
HANEENA8089 <haneenahaneena2013@gmail.com>
2025-09-19 17:40:29 +0530

    product details remove in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Banner from "../components/Banner";
 import ProductList from "../components/ProductList";
 import BestSellers from "../components/BestSellers";
 import Footer from "../components/Footer";
-import ProductDetails from "@/components/productdetails/page";
 
 export default function Homepage() {
   return (
@@ -11,7 +10,6 @@ export default function Homepage() {
       <Header />
       <Banner />
       <BestSellers />
-      <ProductDetails/>
       <ProductList />
       <Footer />
     </div>
