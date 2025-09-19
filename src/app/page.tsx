b8db6c724349a32b77e14fcbba840143a549a9e5
HANEENA8089 <haneenahaneena2013@gmail.com>
2025-09-19 17:40:04 +0530

    product-detailspage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Banner from "../components/Banner";
 import ProductList from "../components/ProductList";
 import BestSellers from "../components/BestSellers";
 import Footer from "../components/Footer";
+import ProductDetails from "@/components/productdetails/page";
 
 export default function Homepage() {
   return (
@@ -10,6 +11,7 @@ export default function Homepage() {
       <Header />
       <Banner />
       <BestSellers />
+      <ProductDetails/>
       <ProductList />
       <Footer />
     </div>
