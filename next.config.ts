26e0bb34bcaff858c5dae036e8c8fd6445ef9484
zemdevwork <zemdevwork@gmail.com>
2025-09-20 10:55:15 +0530

    removed local cofig

--- next.config.ts
+++ next.config.ts
@@ -7,15 +7,6 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "res.cloudinary.com",
         pathname: "/**", // allow all images from Cloudinary
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        pathname: "/**",
-      },{
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_URL as string,
-        pathname: "/**",
       }
     ],
   },
